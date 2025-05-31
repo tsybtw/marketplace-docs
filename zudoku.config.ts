0bdbc248b5c4b698b5b2a2f348aa4e4dd88f77ca
ppleaser <nikitadolzhenko03@gmail.com>
2025-05-31 04:33:29 +0300

    first commit

---
+++ zudoku.config.ts
@@ -0,0 +1,32 @@
+export default {
+  title: "Marketplace API",
+  description: "Документація API для торгової платформи",
+  openapi: {
+    file: "./marketplace-api.yaml"
+  },
+  navigation: [
+    {
+      label: "About",
+      href: "/about"
+    }
+  ],
+  sidebar: {
+    "/": [
+      {
+        type: "doc",
+        id: "installation",
+        label: "Installation"
+      },
+      {
+        type: "doc",
+        id: "authorization",
+        label: "Authorization"
+      },
+      {
+        type: "doc",
+        id: "about",
+        label: "About"
+      }
+    ]
+  }
+};
